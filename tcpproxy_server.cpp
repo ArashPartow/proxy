1bf59eb3d07fc1aa0ba6e3a1bc80af8fc62aba54
Arash Partow <partow@gmail.com>
2012-03-10 11:26:51 +1100

    C++ TCP Proxy Server  http://partow.net/programming/tcpproxy/index.html

--- tcpproxy_server.cpp
+++ tcpproxy_server.cpp
@@ -7,6 +7,39 @@
 //
 // Distributed under the Boost Software License, Version 1.0.
 //
+//
+// Description
+// ~~~~~~~~~~~
+// The objective of the TCP proxy server is to act as an intermediary
+// in order to 'forward' TCP based connections from external clients
+// onto a singular remote server.
+// The communication flow in the direction from the client to the proxy
+// to the server is called the upstream flow, and the communication flow
+// in the direction from the server to the proxy to the client is called
+// the downstream flow. Furthermore the up and down stream connections
+// are consolidated into a single concept known as a bridge.
+// In the event either the downstream or upstream end points disconnect,
+// the proxy server will proceed to disconnect the other end point
+// and eventually destroy the associated bridge.
+//
+// The following is a flow and structural diagram depicting the
+// various elements (proxy, server and client) and how they connect
+// and interact with each other.
+//
+//                                    ---> upstream --->           +---------------+
+//                                                     +---->------>               |
+//                               +-----------+         |           | Remote Server |
+//                     +--------->          [x]--->----+  +---<---[x]              |
+//                     |         | TCP Proxy |            |        +---------------+
+// +-----------+       |  +--<--[x] Server   <-----<------+
+// |          [x]--->--+  |      +-----------+
+// |  Client   |          |
+// |           <-----<----+
+// +-----------+
+//                <--- downstream <---
+//
+//
+
 
 #include <cstdlib>
 #include <cstddef>
@@ -16,75 +49,64 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/bind.hpp>
-#include <boost/function.hpp>
 #include <boost/asio.hpp>
+#include <boost/thread/mutex.hpp>
+
 
 namespace tcp_proxy
 {
    namespace ip = boost::asio::ip;
 
-   class tcp_bridge : public boost::enable_shared_from_this<tcp_bridge>
+   class bridge : public boost::enable_shared_from_this<bridge>
    {
-   private:
-
-      typedef boost::function <void (const boost::system::error_code&, const size_t& )> handler_type;
-      typedef ip::tcp::socket socket_type;
-
    public:
 
-      typedef tcp_bridge type;
-      typedef boost::shared_ptr<type> ptr_type;
+      typedef ip::tcp::socket socket_type;
+      typedef boost::shared_ptr<bridge> ptr_type;
 
-      tcp_bridge(boost::asio::io_service& ios)
-      : client_socket_(ios),
-        remote_socket_(ios)
+      bridge(boost::asio::io_service& ios)
+      : downstream_socket_(ios),
+        upstream_socket_(ios)
       {}
 
-      socket_type& socket()
+      socket_type& downstream_socket()
       {
-         return client_socket_;
+         return downstream_socket_;
       }
 
-      void start(const std::string& remote_host, unsigned short remote_port)
+      socket_type& upstream_socket()
       {
-         remote_socket_.async_connect(
-               ip::tcp::endpoint(
-                     boost::asio::ip::address::from_string(remote_host),
-                     remote_port),
-               boost::bind(&type::handle_remote_connect,
-                     shared_from_this(),
-                     boost::asio::placeholders::error));
+         return upstream_socket_;
       }
 
-      void handle_remote_connect(const boost::system::error_code& error)
+      void start(const std::string& upstream_host, unsigned short upstream_port)
+      {
+         upstream_socket_.async_connect(
+              ip::tcp::endpoint(
+                   boost::asio::ip::address::from_string(upstream_host),
+                   upstream_port),
+               boost::bind(&bridge::handle_upstream_connect,
+                    shared_from_this(),
+                    boost::asio::placeholders::error));
+      }
+
+      void handle_upstream_connect(const boost::system::error_code& error)
       {
          if (!error)
          {
-            remote_write_ = boost::bind(&type::handle_remote_write,
-                  shared_from_this(),
-                  boost::asio::placeholders::error);
+            upstream_socket_.async_read_some(
+                 boost::asio::buffer(upstream_data_,max_data_length),
+                 boost::bind(&bridge::handle_upstream_read,
+                      shared_from_this(),
+                      boost::asio::placeholders::error,
+                      boost::asio::placeholders::bytes_transferred));
 
-            client_write_ = boost::bind(&type::handle_client_write,
-                  shared_from_this(),
-                  boost::asio::placeholders::error);
-
-            remote_read_ = boost::bind(&type::handle_remote_read,
-                  shared_from_this(),
-                  boost::asio::placeholders::error,
-                  boost::asio::placeholders::bytes_transferred);
-
-            client_read_ = boost::bind(&type::handle_client_read,
-                  shared_from_this(),
-                  boost::asio::placeholders::error,
-                  boost::asio::placeholders::bytes_transferred);
-
-            remote_socket_.async_read_some(
-                  boost::asio::buffer(remote_data_, max_data_length),
-                  remote_read_);
-
-            client_socket_.async_read_some(
-                  boost::asio::buffer(client_data_, max_data_length),
-                  client_read_);
+            downstream_socket_.async_read_some(
+                 boost::asio::buffer(downstream_data_,max_data_length),
+                 boost::bind(&bridge::handle_downstream_read,
+                      shared_from_this(),
+                      boost::asio::placeholders::error,
+                      boost::asio::placeholders::bytes_transferred));
          }
          else
             close();
@@ -92,51 +114,61 @@ namespace tcp_proxy
 
    private:
 
-      void handle_client_write(const boost::system::error_code& error)
+      void handle_downstream_write(const boost::system::error_code& error)
       {
          if (!error)
          {
-            remote_socket_.async_read_some(
-                  boost::asio::buffer(remote_data_, max_data_length),
-                  remote_read_);
+            upstream_socket_.async_read_some(
+                 boost::asio::buffer(upstream_data_,max_data_length),
+                 boost::bind(&bridge::handle_upstream_read,
+                      shared_from_this(),
+                      boost::asio::placeholders::error,
+                      boost::asio::placeholders::bytes_transferred));
          }
          else
             close();
       }
 
-      void handle_client_read(const boost::system::error_code& error,
-                              const size_t& bytes_transferred)
+      void handle_downstream_read(const boost::system::error_code& error,
+                                  const size_t& bytes_transferred)
       {
          if (!error)
          {
-            async_write(remote_socket_,
-                  boost::asio::buffer(client_data_,bytes_transferred),
-                  remote_write_);
+            async_write(upstream_socket_,
+                  boost::asio::buffer(downstream_data_,bytes_transferred),
+                  boost::bind(&bridge::handle_upstream_write,
+                        shared_from_this(),
+                        boost::asio::placeholders::error));
          }
          else
             close();
       }
 
-      void handle_remote_write(const boost::system::error_code& error)
+      void handle_upstream_write(const boost::system::error_code& error)
       {
          if (!error)
          {
-            client_socket_.async_read_some(
-                  boost::asio::buffer(client_data_, max_data_length),
-                  client_read_);
+            downstream_socket_.async_read_some(
+                 boost::asio::buffer(downstream_data_,max_data_length),
+                 boost::bind(&bridge::handle_downstream_read,
+                      shared_from_this(),
+                      boost::asio::placeholders::error,
+                      boost::asio::placeholders::bytes_transferred));
          }
          else
             close();
       }
 
-      void handle_remote_read(const boost::system::error_code& error,
-                              const size_t& bytes_transferred)
+      void handle_upstream_read(const boost::system::error_code& error,
+                                const size_t& bytes_transferred)
       {
          if (!error)
          {
-            async_write(client_socket_,
-                  boost::asio::buffer(remote_data_, bytes_transferred),
-                  client_write_);
+            async_write(downstream_socket_,
+                 boost::asio::buffer(upstream_data_,bytes_transferred),
+                 boost::bind(&bridge::handle_downstream_write,
+                      shared_from_this(),
+                      boost::asio::placeholders::error));
          }
          else
             close();
@@ -144,56 +176,47 @@ namespace tcp_proxy
 
       void close()
       {
-         if (client_socket_.is_open()) client_socket_.close();
-         if (remote_socket_.is_open()) remote_socket_.close();
+         boost::mutex::scoped_lock lock(mutex_);
+         if (downstream_socket_.is_open())
+            downstream_socket_.close();
+         if (upstream_socket_.is_open())
+            upstream_socket_.close();
       }
 
-      socket_type client_socket_;
-      socket_type remote_socket_;
+      socket_type downstream_socket_;
+      socket_type upstream_socket_;
 
-      enum { max_data_length = 4096 };
-      char client_data_[max_data_length];
-      char remote_data_[max_data_length];
+      enum { max_data_length = 8192 }; //8KB
+      unsigned char downstream_data_[max_data_length];
+      unsigned char upstream_data_[max_data_length];
 
-      handler_type client_write_;
-      handler_type remote_write_;
-      handler_type client_read_;
-      handler_type remote_read_;
+      boost::mutex mutex_;
 
    public:
 
-      static inline ptr_type create(boost::asio::io_service& ios)
-      {
-         return ptr_type(new type(ios));
-      }
-
       class acceptor
       {
-      private:
-         typedef boost::function<void (const boost::system::error_code&)> handler_type;
-
       public:
 
-         acceptor(boost::asio::io_service& ios,
-                  unsigned short local_port,
-                  unsigned short remote_port,
-                  const std::string& local_host,
-                  const std::string& remote_host)
-         : io_service_(ios),
+         acceptor(boost::asio::io_service& io_service,
+                  const std::string& local_host, unsigned short local_port,
+                  const std::string& upstream_host, unsigned short upstream_port)
+         : io_service_(io_service),
            localhost_address(boost::asio::ip::address_v4::from_string(local_host)),
-           acceptor_(ios,ip::tcp::endpoint(localhost_address,local_port)),
-           remote_port_(remote_port),
-           remote_host_(remote_host)
+           acceptor_(io_service_,ip::tcp::endpoint(localhost_address,local_port)),
+           upstream_port_(upstream_port),
+           upstream_host_(upstream_host)
          {}
 
          bool accept_connections()
          {
             try
             {
-               session_ = type::create(io_service_);
-               acceptor_.async_accept(session_->socket(),
-                     boost::bind(&acceptor::handle_accept,this,
-                           boost::asio::placeholders::error));
+               session_ = boost::shared_ptr<bridge>(new bridge(io_service_));
+               acceptor_.async_accept(session_->downstream_socket(),
+                    boost::bind(&acceptor::handle_accept,
+                         this,
+                         boost::asio::placeholders::error));
             }
             catch(std::exception& e)
             {
@@ -209,15 +232,15 @@ namespace tcp_proxy
          {
             if (!error)
             {
-               session_->start(remote_host_,remote_port_);
+               session_->start(upstream_host_,upstream_port_);
                if (!accept_connections())
                {
-                  std::cerr << "Failed to accept." << std::endl;
+                  std::cerr << "Failure during call to accept." << std::endl;
                }
             }
             else
             {
-               std::cerr << error.message() << std::endl;
+               std::cerr << "Error: " << error.message() << std::endl;
             }
          }
 
@@ -225,9 +248,8 @@ namespace tcp_proxy
          ip::address_v4 localhost_address;
          ip::tcp::acceptor acceptor_;
          ptr_type session_;
-         handler_type handle_;
-         unsigned short remote_port_;
-         std::string remote_host_;
+         unsigned short upstream_port_;
+         std::string upstream_host_;
       };
 
    };
@@ -237,36 +259,35 @@ int main(int argc, char* argv[])
 {
    if (argc != 5)
    {
-      std::cerr << "usage: tcpproxy_server <local host ip> <local port> <remote host ip> <remote port>" << std::endl;
+      std::cerr << "usage: tcpproxy_server <local host ip> <local port> <forward host ip> <forward port>" << std::endl;
       return 1;
    }
 
-   const unsigned short local_port  = static_cast<unsigned short>(::atoi(argv[2]));
-   const unsigned short remote_port = static_cast<unsigned short>(::atoi(argv[4]));
-   const std::string local_host     = argv[1];
-   const std::string remote_host    = argv[3];
+   const unsigned short local_port   = static_cast<unsigned short>(::atoi(argv[2]));
+   const unsigned short forward_port = static_cast<unsigned short>(::atoi(argv[4]));
+   const std::string local_host      = argv[1];
+   const std::string forward_host    = argv[3];
 
    boost::asio::io_service ios;
 
    try
    {
-      tcp_proxy::tcp_bridge::acceptor acceptor(ios,
-                                               local_port,remote_port,
-                                               local_host,remote_host);
+      tcp_proxy::bridge::acceptor acceptor(ios,
+                                           local_host, local_port,
+                                           forward_host, forward_port);
       acceptor.accept_connections();
       ios.run();
    }
    catch(std::exception& e)
    {
-      std::cerr << e.what() << std::endl;
+      std::cerr << "Error: " << e.what() << std::endl;
       return 1;
    }
 
    return 0;
 }
 
-
 /*
- * Note: build command:
+ * [Note] On posix systems the tcp proxy server build command is as follows:
  * c++ -pedantic -ansi -Wall -Werror -O3 -o tcpproxy_server tcpproxy_server.cpp -L/usr/lib -lstdc++ -lpthread -lboost_thread -lboost_system
  */
